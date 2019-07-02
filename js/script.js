77dfc9f581315fa228ad867f5d50ae3a54a715e2
waelzegneni <wael-msk2@live.fr>
2019-07-02 14:53:37 +0100

    my 3rd check point

---
+++ js/script.js
@@ -0,0 +1,54 @@
+
+var b = 0;
+document.getElementById("bold").addEventListener("click", function () {
+    if (b == 0) {
+        document.getElementById("textArea").style.fontWeight = "bolder";
+        b = 1;
+    }
+    else {
+        document.getElementById("textArea").style.fontWeight = "normal";
+        b = 0;
+    }
+}
+);
+
+var i = 0;
+document.getElementById("italic").addEventListener("click", function () {
+    if (i == 0) {
+        document.getElementById("textArea").style.fontStyle = "italic";
+        i = 1;
+    }
+
+    else {
+        document.getElementById("textArea").style.fontStyle = "";
+        i = 0;
+    }
+}
+);
+
+var u = 0 ;
+document.getElementById("underline").addEventListener("click", function () {
+    if(u == 0 ) {
+    document.getElementById("textArea").style.textDecoration = "underline";
+    u=1;
+    }
+    else{
+        document.getElementById("textArea").style.textDecoration = "none";
+        u=0;
+    }
+}
+);
+
+
+
+ function handleFontSize()
+{
+    var fontsize= document.getElementById("fontsize").value;
+    document.getElementById("textArea").style.fontSize=fontsize;
+}
+
+function handleTextFamily()
+{
+    var fontFamily= document.getElementById("textfamily").value;
+    document.getElementById("textArea").style.fontFamily= fontFamily;
+}
