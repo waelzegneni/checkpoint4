93812b0b177468a3b27789efc5fdc6fb4c290cde
waelzegneni <wael-msk2@live.fr>
2019-07-03 15:30:44 +0100

    checkpoint4

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 
+$(".dialog").hide();
+$(".dialog1").hide();
+
+
 var b = 0;
 document.getElementById("bold").addEventListener("click", function () {
     if (b == 0) {
@@ -26,29 +30,40 @@ document.getElementById("italic").addEventListener("click", function () {
 }
 );
 
-var u = 0 ;
+var u = 0;
 document.getElementById("underline").addEventListener("click", function () {
-    if(u == 0 ) {
-    document.getElementById("textArea").style.textDecoration = "underline";
-    u=1;
+    if (u == 0) {
+        document.getElementById("textArea").style.textDecoration = "underline";
+        u = 1;
     }
-    else{
+    else {
         document.getElementById("textArea").style.textDecoration = "none";
-        u=0;
+        u = 0;
     }
 }
 );
 
 
 
- function handleFontSize()
-{
-    var fontsize= document.getElementById("fontsize").value;
-    document.getElementById("textArea").style.fontSize=fontsize;
+function handleFontSize() {
+    var fontsize = document.getElementById("fontsize").value;
+    document.getElementById("textArea").style.fontSize = fontsize;
 }
 
-function handleTextFamily()
-{
-    var fontFamily= document.getElementById("textfamily").value;
-    document.getElementById("textArea").style.fontFamily= fontFamily;
+function handleTextFamily() {
+    var fontFamily = document.getElementById("textfamily").value;
+    document.getElementById("textArea").style.fontFamily = fontFamily;
 }
+
+
+
+$(".button1").click(function () {
+    $(".dialog").dialog();
+}
+);
+
+
+$(".button1").click(function () {
+    $(".dialog1").dialog();
+}
+);
